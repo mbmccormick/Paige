20c47d0b787984f3a162f18861c226abe0e88d39
Matt McCormick <mbmccormick@gmail.com>
2012-01-04 14:11:14 -0500

    Adding database changes.

--- db/database.sql
+++ db/database.sql
@@ -1,7 +1,19 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phonenumber` varchar(10) NOT NULL,
+  `stripeid` varchar(50) NOT NULL,
+  `stripeplan` varchar(50) NOT NULL,
+  `createddate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accountid` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
@@ -10,4 +22,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   `isadministrator` tinyint(11) NOT NULL,
   `createddate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
