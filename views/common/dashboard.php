058e3daf6d810465526e13086dcebfc4dc2a5447
Matt McCormick <mbmccormick@gmail.com>
2012-01-20 13:20:36 -0500

    Update views/common/dashboard.php

--- views/common/dashboard.php
+++ views/common/dashboard.php
@@ -1,5 +1,18 @@
 <div class="row">
     <div class="span14">
-        Welcome to <?=ApplicationName?>! If you are seeing this page, then you have successfully signed in to this application.
+        <form action="<?=option('base_uri')?>members/add" method="post" class="form-stacked">
+            <fieldset>
+                <div class="clearfix">
+                    <label for="name">Message</label>
+                    <div class="input">
+                        <textarea class="xlarge" id="message" name="message" rows="2"></textarea>
+                    </div>
+                </div>
+            </fieldset>
+            <br />
+            <div>                
+                <button type="submit" class="btn large success">Page Team</button>
+            </div>
+        </form>
     </div>
 </div>
