9f292a6d7a67e9f07e9548ab49e126694f5e4b57
Matt McCormick <mbmccormick@gmail.com>
2012-01-20 13:04:48 -0500

    Update views/accounts/add.php

--- views/accounts/add.php
+++ views/accounts/add.php
@@ -14,13 +14,13 @@
                         <input class="xlarge" id="email" name="email" size="30" type="text">
                     </div>
                 </div>
-				<div class="clearfix">
+                <div class="clearfix">
                     <label for="areacode">Area Code</label>
                     <div class="input">
                         <input class="xlarge" id="areacode" name="areacode" size="30" type="text">
                     </div>
                 </div>
-				<br />
+                <br />
                 <div class="clearfix">
                     <label for="plan">Plan</label>
                     <div class="input">
@@ -31,7 +31,7 @@
                         </select>
                     </div>
                 </div>
-				<br />
+                <br />
                 <div class="clearfix">
                     <label for="cardnumber">Credit Card Number</label>
                     <div class="input">
@@ -42,31 +42,31 @@
                     <label for="cardexpmonth">Expiration</label>
                     <div class="input">
                         <select class="small" id="cardexpmonth" name="cardexpmonth" autocomplete="off">
-							<option value="01">01</option>
-							<option value="02">02</option>
-							<option value="03">03</option>
-							<option value="04">04</option>
-							<option value="05">05</option>
-							<option value="06">06</option>
-							<option value="07">07</option>
-							<option value="08">08</option>
-							<option value="09">09</option>
-							<option value="10">10</option>
-							<option value="11">11</option>
-							<option value="12">12</option>
-						</select>
-						<select class="small" id="cardexpyear" name="cardexpyear" autocomplete="off">
-							<option value="2012">2012</option>
-							<option value="2013">2013</option>
-							<option value="2014">2014</option>
-							<option value="2015">2015</option>
-							<option value="2016">2016</option>
-							<option value="2017">2017</option>
-							<option value="2018">2018</option>
-							<option value="2019">2019</option>
-							<option value="2020">2020</option>
-							<option value="2021">2021</option>
-						</select>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                        <select class="small" id="cardexpyear" name="cardexpyear" autocomplete="off">
+                            <option value="2012">2012</option>
+                            <option value="2013">2013</option>
+                            <option value="2014">2014</option>
+                            <option value="2015">2015</option>
+                            <option value="2016">2016</option>
+                            <option value="2017">2017</option>
+                            <option value="2018">2018</option>
+                            <option value="2019">2019</option>
+                            <option value="2020">2020</option>
+                            <option value="2021">2021</option>
+                        </select>
                     </div>
                 </div>
                 <div class="clearfix">
