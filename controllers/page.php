89e3e4cfba3310efa40dff1d4a498f03674993e4
martellaj <martellaj@gmail.com>
2012-01-21 18:02:43 -0500

    Queues unconfirmed messages and sends out texts to members who are opted in.

--- controllers/page.php
+++ controllers/page.php
@@ -6,7 +6,7 @@
         
         if (strlen($_GET[message]) > 120)
         {
-            die("Message must be 120 characters or less.")
+            die("Message must be 120 characters or less.");
             exit;
         }
 
@@ -24,10 +24,15 @@
         // initialize twilio client
         $twilio = new Services_Twilio('AC5057e5ab36685604eecc9b1fdd8528e2', '309e6930d27b624bbfaa45dac382c6ae');
         
+		if (isset($_GET[attempt]) == true)
+			$attempt = $_GET[attempt] + 1;
+		else
+			$attempt = 1;
+			
         $call = $twilio->account->calls->create(
             $_SESSION['CurrentAccount_PhoneNumber'],
             $member[phonenumber],
-            "http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step2&message=" . urlencode($_GET[message])
+            "http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step2&attempt=" . $attempt . "&message=" . urlencode($_GET[message])
         );
     }
 
@@ -37,7 +42,7 @@
 
         echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
         echo "<Response>\n";
-        echo "<Gather timeout='20' action='http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step3' method='POST' numDigits='1'>\n";
+        echo "<Gather timeout='20' action='http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step3&amp;attempt=" . urlencode($_GET[attempt]) . "&amp;message=" . urlencode($_GET[message]) . "' method='POST' numDigits='1'>\n";
         echo "<Say voice='woman'>Hello, this is an automated page from " . $_SESSION['CurrentAccount_Name'] . ".</Say>\n";
         echo "<Say voice='woman'>" . $_GET[message] . "</Say>\n";
         echo "<Say voice='woman'>Press one now to confirm that you have received this message.</Say>\n";
@@ -58,10 +63,57 @@
 		}
 		else 
 		{
-			echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
-			echo "<Response>\n";
-			echo "<Say voice='woman'>Your page was not confirmed. We will attempt to page you again in 15 minutes.</Say>\n";
-			echo "</Response>\n";
+			if ($_GET[attempt] == 1 || $_GET[attempt] == 2) 
+			{
+				echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
+				echo "<Response>\n";
+				echo "<Say voice='woman'>Your message was not confirmed. We will try again in fifteen minutes.</Say>\n";
+				echo "</Response>\n";
+				
+				// add message to queue
+				$now = date("Y-m-d H:i:s");
+				
+				$url = "http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step1&attempt=" . $_GET[attempt] . "&message=" . $_GET[message];
+				$duedatetime = date("Y-m-d H:i:s", strtotime('+15 minutes'));
+				$createdtime = $now;
+				
+				$sql = "INSERT INTO queue (duedatetime, url, createddate) VALUES ('" . $duedatetime . "', '" . $url . "', '" . $createdtime . "')";
+				mysql_query($sql);
+				
+				// if opted in, send a text
+				
+				// lookup the on-call member
+				$onCall = mysql_query("SELECT * FROM schedule WHERE startdate <= '" . $now . "' AND accountid='" . $_SESSION['CurrentAccount_ID'] . "' ORDER BY startdate DESC");
+				$shift = mysql_fetch_array($onCall);
+				
+				$result = mysql_query("SELECT * FROM member WHERE id='" . $shift[memberid] . "'");
+				$member = mysql_fetch_array($result);
+				
+				if ($member[isoptedin] == 1) 
+				{
+					// send text
+					$twilio = new Services_Twilio('AC5057e5ab36685604eecc9b1fdd8528e2', '309e6930d27b624bbfaa45dac382c6ae');
+					
+					$message = $twilio->account->sms_messages->create(
+					  $_SESSION['CurrentAccount_PhoneNumber'],
+					  $member[phonenumber], // Text this number
+					  $_GET[message] . " Reply \"confirm\" to confirm this page."
+					);
+				}
+				else
+				{
+					// exit
+				}
+			}
+			else 
+			{
+				echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
+				echo "<Response>\n";
+				echo "<Say voice='woman'>You should never hear this.</Say>\n";
+				echo "</Response>\n";
+			}
+			
+			// leave a voicemail
 		}
 	}
 	
