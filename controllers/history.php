865097f7db698f1480ed3cdcade6b704f4df0e34
martellaj <martellaj@gmail.com>
2012-01-21 15:05:10 -0500

    Shows page history.

--- controllers/history.php
+++ controllers/history.php
@@ -4,7 +4,49 @@
     {
         Security_Authorize();
 
+		// gets all tuples from history table
+        $result = mysql_query("SELECT * FROM history WHERE accountid='" . $_SESSION['CurrentAccount_ID'] . "' ORDER BY createddate DESC");
+
+		// iterates through result
+		while($row = mysql_fetch_array($result)) 
+		{
+			$body .= "<tr>\n";
+       
+            $body .= "<td>\n";		
+			$name = mysql_query("SELECT name FROM member WHERE id=" . $row[memberid]);
+			$name = mysql_fetch_array($name);
+            $body .= $name[name];
+            $body .= "</td>\n";
+            $body .= "<td>\n";
+			$body .= $row[message];
+            $body .= "</td>\n";
+			$body .= "<td>\n";
+			$body .= date('F d, Y \a\t g:ia', strtotime($row[createddate]));
+            $body .= "</td>\n";
+            $body .= "<td>\n";
+					
+			if ($row[medium] == 0) 
+				$body .= "Web";
+			else if ($row[medium] == 1)
+				$body .= "Telephone";
+			else if ($row[medium] == 2)
+				$body .= "Hook";
+
+            $body .= "</td>\n";
+			
+            $body .= "</tr>\n";
+		}
+		
+		if (mysql_num_rows($result) == 0)
+        {
+            $body .= "<tr>\n";
+            $body .= "<td colspan='4'>There has not been any pages sent from this account.</td>\n";
+            $body .= "</tr>\n";
+        }
         
+        set("title", "History");
+        set("body", $body);
+        return html("history/list.php");
     }
 
 ?>
--- views/history/list.php
+++ views/history/list.php
@@ -3,22 +3,20 @@
         <table class="bordered-table">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th style="width: 100px;">Actions</th>
+                    <th>Member</th>
+					<th>Message</th>
+					<th>Date</th>
+					<th>Medium</th>
                 </tr>
             </thead>
             <tbody>
                 <?=$body?>
             </tbody>
         </table>
-        <div class="well">
-            <a href="<?=option('base_uri')?>members/add" class="btn primary">New Member</a>
-        </div>
     </div>
     <div class="span4">
         <h5>Page Description</h5>
-        <p>This page shows the list of team members currently setup for the application. This list allows you to view a team members's information or create a new team member.</p>
+        <p>This page shows the list of pages delivered through the application.</p>
         <br />
     </div>
 </div>
