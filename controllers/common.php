05ed4f137d6dd47bedbba4289b46de110409c761
Matt McCormick <mbmccormick@gmail.com>
2012-01-21 16:41:34 -0500

    Updating dashboard display.

--- controllers/common.php
+++ controllers/common.php
@@ -3,8 +3,21 @@
     function common_dashboard()
     {
         Security_Authorize();
+
+        $result = mysql_query("SELECT * FROM history WHERE accountid='" . $_SESSION['CurrentAccount_ID'] . "' ORDER BY createddate DESC LIMIT 3");
+        while($row = mysql_fetch_array($result))
+        {
+            $result2 = mysql_query("SELECT * FROM member WHERE id='" . $row[memberid] . "'");
+            $member = mysql_fetch_array($result2);
+
+            $history .= "<div class='history-item'>\n";
+            $history .= "<b><a href='" . option('base_uri') . "members/$member[id]'>" . $member[name] . "</a> was paged " . FriendlyDate(1, strtotime($row[createddate])) . " via web</b><br />\n";
+            $history .= $row[message] . "<br />\n";
+            $history .= "</div><br />\n";
+        }
         
         set("title", "Dashboard");
+        set("history", $history);
         return html("common/dashboard.php");
     }
 
@@ -12,7 +25,10 @@
     {
     	Security_Authorize();
 
-    	RequestUrl("http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step1&message=" . urlencode($_POST[message]));
+        if ($_POST[recipient] == 1)
+    	   RequestUrl("http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step1&message=" . urlencode($_POST[message]));
+        else
+            RequestUrl("http://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step1&team=true&message=" . urlencode($_POST[message]));
         
         header("Location: /&success=Your page was sent successfully!");
         exit;
--- views/common/dashboard.php
+++ views/common/dashboard.php
@@ -1,10 +1,19 @@
 <div class="row">
     <div class="span7">
-        <h3>Page Team</h3>
+        <h3>Send Page</h3>
         <span class="help-block">Send a message to your on-call team from the web.</span>
         <br />
         <form action="<?=option('base_uri')?>" method="post" class="form-stacked" style="padding-left: 0px;">
             <fieldset>
+                <div class="clearfix">
+                    <label for="name">Recipient</label>
+                    <div class="input">
+                        <select class="xlarge" id="recipient" name="recipient">
+                            <option value="1">Current on-call team member</option>
+                            <option value="2">All team members</option>
+                        </select>
+                    </div>
+                </div>
                 <div class="clearfix">
                     <label for="name">Message</label>
                     <div class="input">
@@ -17,7 +26,7 @@
             </fieldset>
             <br />
             <div>                
-                <button type="submit" class="btn success">Page Team</button>
+                <button type="submit" class="btn success">Send Page</button>
             </div>
         </form>
     </div>
@@ -25,5 +34,6 @@
         <h3>Recent History</h3>
         <span class="help-block">View the recent pages that have been sent.</span>
         <br />
+        <?=$history?>
     </div>
 </div>
