dadcb8272d1da2f85c5e87aba2b6defe9f428979
Matt McCormick <mbmccormick@gmail.com>
2012-01-21 23:44:17 -0500

    Adjusting content.

--- views/common/home.php
+++ views/common/home.php
@@ -5,6 +5,7 @@
         <h4 style="color: #BFBFBF;">Web-based personnel paging made easy.</h4>
         <p style="line-height: 1.5em;">Paige is a simple web application that enables you to page your IT support department, property management personnel, medical staff, or whatever mission-critical teams your business employs.</p>
         <p style="line-height: 1.5em;">Analog pagers are costly, unreliable, and redundant. Paige makes use of your team's existing mobile phones to deliver important messages and get timely results. You can easily integrate Paige with your existing monitoring systems to ensure instant delivery of your pages.</p>
+        <p style="line-height: 1.5em;">We have a Service Level Agreement to ensure 99.999% uptime, so that you can trust your mission-critical systems with Paige.</p>
     	<br />
     	<br />
     	<div class="well" style="padding-bottom: 10px;">
@@ -13,6 +14,6 @@
 		</div>
     </div>
     <div class="span6" style="text-align: right;">
-    	<img src="http://i.istockimg.com/file_thumbview_approve/5550637/2/stock-photo-5550637-female-robot.jpg" />
+    	<img src="/public/img/robot.jpg" />
     </div>
 </div>
