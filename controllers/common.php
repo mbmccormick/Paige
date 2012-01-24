6ee50d11ac09826dd69d2713d8dea85e0a0c4237
Matt McCormick <mbmccormick@gmail.com>
2012-01-24 14:45:47 -0500

    Replacing tabs with spaces.

--- controllers/common.php
+++ controllers/common.php
@@ -1,6 +1,6 @@
 <?php
-	$i = 1;
-	
+    $i = 1;
+    
     function common_dashboard()
     {
         if ($_SESSION['CurrentAccount_ID'] == null)
@@ -30,20 +30,20 @@
             $member = mysql_fetch_array($result);
 
             $oncall = $member[name];
-			
-			$i = 2;
-			$input .= "<input type=\"hidden\" name=\"team1\" value=\"" . $member[id] . "\">";
-			
-			// get the other team members
-			$teamQuery = mysql_query("SELECT * FROM member WHERE accountid='" . $_SESSION['CurrentAccount_ID'] . "' and id!='" . $member[id] . "'");
-			while($team = mysql_fetch_array($teamQuery))
-			{
-				$options .= "<option value=\"" . $i . "\">" . $team[name] . "</option>";
-				$inc = "team" . $i;
-				$input .= "<input type=\"hidden\" name=\"" . $inc . "\" value=\"" . $team[id] . "\">";  
-				//$options .= "<option value=\"" . $i . "\">" . $ids[$i] . "</option>";
-				$i++;
-			}
+            
+            $i = 2;
+            $input .= "<input type=\"hidden\" name=\"team1\" value=\"" . $member[id] . "\">";
+            
+            // get the other team members
+            $teamQuery = mysql_query("SELECT * FROM member WHERE accountid='" . $_SESSION['CurrentAccount_ID'] . "' and id!='" . $member[id] . "'");
+            while($team = mysql_fetch_array($teamQuery))
+            {
+                $options .= "<option value=\"" . $i . "\">" . $team[name] . "</option>";
+                $inc = "team" . $i;
+                $input .= "<input type=\"hidden\" name=\"" . $inc . "\" value=\"" . $team[id] . "\">";  
+                //$options .= "<option value=\"" . $i . "\">" . $ids[$i] . "</option>";
+                $i++;
+            }
 
             $result = mysql_query("SELECT * FROM history WHERE accountid='" . $_SESSION['CurrentAccount_ID'] . "' ORDER BY createddate DESC LIMIT 3");
             while($row = mysql_fetch_array($result))
@@ -76,20 +76,20 @@
             set("title", "Dashboard");
             set("oncall", $oncall);
             set("history", $history);
-			set("i", $i);
-			set("options", $options);
-			set("input", $input);
+            set("i", $i);
+            set("options", $options);
+            set("input", $input);
             return html("common/dashboard.php");
         }
     }
 
     function common_dashboard_post()
     {
-    	Security_Authorize();
-		
+        Security_Authorize();
+        
         if ($_POST[recipient] != $_POST[i])
         {
-    	    $now = AccountTime();
+            $now = AccountTime();
         
             // lookup the on-call member
             /*$result = mysql_query("SELECT * FROM schedule WHERE startdate <= '" . $now . "' AND accountid='" . $_SESSION['CurrentAccount_ID'] . "' ORDER BY startdate DESC");
@@ -97,13 +97,13 @@
 
             $result = mysql_query("SELECT * FROM member WHERE id='" . $shift[memberid] . "'");
             $member = mysql_fetch_array($result);*/
-			
-			//$result = mysql_query("SELECT * FROM member WHERE id='" . $ids[$i] . "'");
-			//$member = mysql_fetch_array($result);
+            
+            //$result = mysql_query("SELECT * FROM member WHERE id='" . $ids[$i] . "'");
+            //$member = mysql_fetch_array($result);
 
-			$idVal = "team" . $_POST[recipient]; 
+            $idVal = "team" . $_POST[recipient]; 
             LogHistory($_POST[$idVal], $_POST[message], 1);
-			
+            
             RequestUrl("https://paigeapp.com/page/" . $_SESSION['CurrentAccount_ID'] . "/step1&message=" . urlencode($_POST[message]) . "&memberid=" . urlencode($_POST[$idVal]));
         }
         else
@@ -129,19 +129,19 @@
 
     function common_execute()
     {
-    	$result1 = mysql_query("SELECT * FROM account ORDER BY id");
+        $result1 = mysql_query("SELECT * FROM account ORDER BY id");
         while($row1 = mysql_fetch_array($result1))
         {
-        	Security_Refresh($row1[id]);
-        	
-	        $now = AccountTime();
-	        
-	        $result2 = mysql_query("SELECT * FROM queue WHERE duedatetime <= '" . $now . "' AND accountid='" . $row1[id] . "'");
-	        while($row2 = mysql_fetch_array($result2))
-	        {
-	            RequestUrl($row2[url] . "&queue=true");
-	            mysql_query("DELETE FROM queue WHERE id='" . $row2[id] . "'");
-	        }
+            Security_Refresh($row1[id]);
+            
+            $now = AccountTime();
+            
+            $result2 = mysql_query("SELECT * FROM queue WHERE duedatetime <= '" . $now . "' AND accountid='" . $row1[id] . "'");
+            while($row2 = mysql_fetch_array($result2))
+            {
+                RequestUrl($row2[url] . "&queue=true");
+                mysql_query("DELETE FROM queue WHERE id='" . $row2[id] . "'");
+            }
         }
     }
 
