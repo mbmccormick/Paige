d19e6e1fffeb7f7d588a10bdedcc85308884c5a2
Matt McCormick <mbmccormick@gmail.com>
2012-02-03 14:03:57 -0500

    Configuration file is now in source control.

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    // database configuration
+    define(Server, "data.mccormicktechnologies.com");
+    define(Username, "mccormick_tech");
+    define(Password, "mcc0rm1ck_t3ch!");
+    define(Database, "mccormicktech_ops");
+    
+    // application settings
+    define(ApplicationName, "Paige");
+    define(EmailAddress, "system@paigeapp.com");
+    define(Version, "2012.02.03");
+    
+?>
