0a2c4a3f40bbfcb4015c0e7419d799e339b367c2
Matt McCormick <mbmccormick@gmail.com>
2012-02-03 14:59:57 -0500

    Updating front-end website.

--- views/common/home.php
+++ views/common/home.php
@@ -1,14 +1,15 @@
 <div class="hero-unit">
     <h1>Hello, I'm Paige.</h1>
-    <p>Paige is a simple web application that enables you to page your IT support department, property management personnel, medical staff, or whatever mission-critical teams your business employs.</p>
+    <p>Paige is a simple web application that allows businesses to ditch analog pagers and move their paging service to the web.</p>
     <p><a href="/register" class="btn btn-large btn-success">Get Started Now</a></p>
 </div>
 <div class="row">
     <div class="span5">
         <h3>Web-based personnel paging made easy</h3>
-        <p style="line-height: 1.5em;">Paige is a simple web application that enables you to page your IT support department, property management personnel, medical staff, or whatever mission-critical teams your business employs.</p>
-        <p style="line-height: 1.5em;">Analog pagers are costly, unreliable, and redundant. Paige makes use of your team's existing mobile phones to deliver important messages and get timely results. You can easily integrate Paige with your existing monitoring systems to ensure instant delivery of your pages.</p>
-        <p style="line-height: 1.5em;">Paige is backed by a 99.999% uptime Service Level Agreement, so you can ensure that your paging systems are available when you need them.</p>
+        <p style="line-height: 1.5em;">Paige makes it easy to outsource your business’s personnel paging needs. In the past, companies had to pay costly fees to run their pager fleet, staff their answering service, and manually escalate cases to other on-call personnel. Paige streamlines this process by moving everything to the web.</p>
+        <p style="line-height: 1.5em;">When you sign up for Paige, a phone number is registered for your new paging service. This number is used to page your team members when an incident arises. You can schedule your on-call team member within our application and the system will automatically page the person on-call.</p>
+        <p style="line-height: 1.5em;">If a team member misses a page, our service will automatically retry after 10 minutes. If they miss the second attempt, a third attempt is made after 10 minutes. If the on-call team member misses the third page attempt, Paige will notify the whole team of this incident.</p>
+        <p style="line-height: 1.5em;">You can page your team through Paige’s web interface, your private telephone menu, or through our simple API.</p>
     </div>
     <div class="span3" style="text-align: right;">
     	<img src="/public/img/robot.jpg" />
